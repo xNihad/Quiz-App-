bb4478b9e8f69fea834ca7771fe9b8577125bbc7
xNihad <nxankishiyev@std.beu.edu.az>
2024-05-16 05:40:54 +0400

    Adding Homepage

---
+++ app/src/main/java/com/example/quizapp/HomePageActivity.kt
@@ -0,0 +1,25 @@
+package com.example.quizapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.quizapp.databinding.ActivityHomePageBinding
+
+class HomePageActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityHomePageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+       binding.debutton.setOnClickListener{
+           val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    }
+}
--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -3,11 +3,7 @@ package com.example.quizapp
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.postDelayed
 import com.example.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
